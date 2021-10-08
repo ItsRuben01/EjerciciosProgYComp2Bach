fa3af2c8dac1408ceae6e577666f62dfe937a378
ItsRuben01 <rubcastru@gmail.com>
2021-10-08 11:32:30 +0200

    Mi primer ejercicio

---
+++ src/main/java/Capitulo01/bloque01/Ejercicio10.java
@@ -0,0 +1,23 @@
+package Capitulo01.bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		
+			Scanner sc = new Scanner(System.in); // Lee el teclado
+	
+			System.out.println("Introduzca un número:");
+			int numEntero = sc.nextInt();
+			
+			System.out.println("Introduzca un número flotante:");
+			float numFloat = sc.nextFloat();
+			
+			System.out.println("Introduzca un número doble:");
+			double numdouble = sc.nextDouble();
+	
+			
+	}
+
+}
