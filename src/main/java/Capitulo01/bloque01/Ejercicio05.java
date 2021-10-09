3a0fa45c4e01b560ddb6a95ac54cf34ab565a631
Equipo <Equipo@192.168.1.152>
2021-10-09 10:24:51 +0200

    Ejercicio 5

---
+++ src/main/java/Capitulo01/bloque01/Ejercicio05.java
@@ -0,0 +1,27 @@
+package Capitulo01.bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+    
+	System.out.println("Introduzca 2 números a continuación");
+	
+	System.out.println("Introduzca el primer número:");
+    double primero = sc.nextDouble();
+    
+    System.out.println("Introduzca el segundo número:");
+    double segundo = sc.nextDouble();
+		
+    System.out.println();
+    
+    
+    
+    
+    
+	}
+
+}
