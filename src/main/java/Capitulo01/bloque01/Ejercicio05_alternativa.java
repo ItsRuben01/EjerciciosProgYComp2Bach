0b4d5c2e8e7d95f1d20d20cce9931ca62a418607
Equipo <Equipo@192.168.1.152>
2021-10-09 16:35:47 +0200

    bloque01

--- src/main/java/Capitulo01/bloque01/Ejercicio05_alternativa.java
+++ src/main/java/Capitulo01/bloque01/Ejercicio05_alternativa.java
@@ -2,7 +2,7 @@ package Capitulo01.bloque01;
 
 import java.util.Scanner;
 
-public class Ejercicio05 {
+public class Ejercicio05_alternativa {
 
 	public static void main(String[] args) {
 		
@@ -17,9 +17,14 @@ public class Ejercicio05 {
     double segundo = sc.nextDouble();
     
     System.out.println("Las variables sin modificar son → " + primero +" ; " + segundo); // Antes del cambio
-   
     
-    System.out.println("Las variables modificadas son → " + segundo + " ; " + primero); 
+    double auxiliar = 5; // variable escondida
+    double auxiliar2 = -3; // 2 variable escondida
+    
+    double mod = auxiliar + primero;
+    double mod2 = auxiliar2 + segundo;
+    
+    System.out.println("Las variables modificadas son → " + mod + " ; " + mod2); 
     
     
     
