7fd659c3c474a4e8cf337a437bdb81c518ee6a0c
ItsRuben01 <rubcastru@gmail.com>
2021-10-22 11:31:14 +0200

    bloque01

---
+++ src/main/java/Capitulo01/bloque01/Ejercicio06.java
@@ -0,0 +1,28 @@
+package Capitulo01.bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+
+	
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	int num1 , num2;
+	
+	System.out.println("Introdzuca un número : ");
+    num1 = sc.nextInt();
+    
+    System.out.println("Introduzca su segundo número : ");
+	num2 = sc.nextInt();
+	
+	
+	num1 > num2;
+	System.out.println("El número mayor es : ");
+	
+	
+	
+	}
+	
+}
