8e7f11626ebae5340279c240b496bc10bab4f2ba
Equipo <Equipo@192.168.1.152>
2021-11-04 22:05:25 +0100

    bloque02

---
+++ src/main/java/Capitulo02_condicionales/bloque02/Ejercicio04_condicionalesmonedas.java
@@ -0,0 +1,11 @@
+package Capitulo02_condicionales.bloque02;
+
+public class Ejercicio04_condicionalesmonedas {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Esta en proceso...");
+	
+	}
+
+}
