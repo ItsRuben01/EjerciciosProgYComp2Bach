3a0efcdcf4630859deae665dd9cb4b47861b4976
Equipo <Equipo@192.168.1.133>
2021-11-15 22:02:54 +0100

    bloque02

--- src/main/java/Capitulo03/bloque02/Ejercicio02_buclesfor.java
+++ src/main/java/Capitulo03/bloque02/Ejercicio02_buclesfor.java
@@ -7,19 +7,37 @@ public class Ejercicio02_buclesfor {
 
 	public static void main(String[] args) {
 Scanner sc = new Scanner (System.in);
-int suma = 0;
 
-System.out.println("Introduzca las repeticiones que quiera hacer: ");
-int rep = sc.nextInt();
+int media = 0, num1,num2,cN;
 
-	for (int i = 0; i < rep; i++) {
+System.out.println("Introduzca la cantidad de números que desee: ");
+	cN = sc.nextInt();
+
+	for (int i = 0; i < cN; i++) {
 	System.out.println("Introduzca el número que quiere: ");
-	int num1 = sc.nextInt();
-	if (suma > 10) {
-    suma = suma + num1;		
+	num1 = sc.nextInt();
+	System.out.println("Introduzca el número que quiere: ");
+	num2 = sc.nextInt();
+	
+	if (num1 < num2 ) {
+		System.out.println("El número más grande es =>" + num2 );	
 	}
+	else {
+		if(num1 > num2) {
+		System.out.println("El número más grande es =>" + num1);
+	}	
+		else {
+		System.out.println("Los dos números son iguales => " + num1 + " y " + num2);
+	     }
 	}
-	System.out.println("La suma vale: " + suma);
+	}		
 	}
 
 }
+
+
+
+
+
+
+
--- src/main/java/Capitulo03/bloque02/Ejercicio03_buclesfor.java
+++ src/main/java/Capitulo03/bloque02/Ejercicio03_buclesfor.java
@@ -7,26 +7,31 @@ public class Ejercicio03_buclesfor {
 
 	public static void main(String[] args) {
 Scanner sc = new Scanner (System.in);
-int suma = 0, contadorp = 0, contadorn = 0;
 
+float ec1,ec2;
+int valor = 2;
 System.out.println("Introduzca las repeticiones que quiera hacer: ");
 int rep = sc.nextInt();
 
 	for (int i = 0; i < rep; i++) {
 	System.out.println("Introduzca el número que quiere: ");
-	int num1 = sc.nextInt();
+	float num1 = sc.nextFloat();
 	
-	if (suma > 0) {
-	    contadorp++;
-	    
-		}	
-	if (suma < 0) {
-		contadorn++;
-		    
+	ec1 = (num1 % valor);
+	ec2 = (num1 / valor);
+	
+	System.out.println("El número es: ");
+	if (ec1 == 0) {
+
+		System.out.println("PAR => " + ec1);
+	}
+	else {
+	
+		if (ec2 > 0) {
+	    System.out.println("IMPAR => " + ec2);
 		}
 	}
-	System.out.println("Los número positivos son: " + contadorp );
-	System.out.println("Los número negativos son: " + contadorn );
+	}
 	}
 
 }
