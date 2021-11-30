ea21ee09086c999ee5959da3059bedfc5bc32d42
ItsRuben01 <rubcastru@gmail.com>
2021-11-30 10:31:13 +0100

    bloque02

---
+++ src/main/java/Capitulo05_arrays/bloque02/Ejercicio01_arrays.java
@@ -0,0 +1,15 @@
+package Capitulo05_arrays.bloque02;
+
+public class Ejercicio01_arrays {
+
+	public static void main(String[] args) {
+		
+		int array[] = new int[150];
+		float suma = 0,media = 0,mayor = 0,menor = 0;
+	
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) Math.round(Math.random() * 100);
+		
+	  }
+	}
+}
