62a0a0a994aba2c7b192f8ae42a22f663f29eaed
ItsRuben01 <rubcastru@gmail.com>
2021-12-10 11:30:52 +0100

    bloque04

---
+++ src/main/java/Capitulo05_arrays/bloque04/Ejercicio01_arrays.java
@@ -0,0 +1,20 @@
+package Capitulo05_arrays.bloque04;
+
+import java.util.Iterator;
+
+public class Ejercicio01_arrays {
+
+	public static void main(String[] args) {
+		int firstarray[] = new int [150];
+		int secondarray[] = new int [150];
+		int thirdarray[] = new int [150];
+		
+		
+		
+		
+		
+		
+	
+
+}
+}
