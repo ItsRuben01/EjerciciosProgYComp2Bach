22d4634da7c6eba50d2fef711d9f76b94d85f060
ItsRuben01 <rubcastru@gmail.com>
2021-12-10 11:30:41 +0100

    bloque03

--- src/main/java/Capitulo05_arrays/bloque03/Ejercicio02_algoritmoBurbuja.java
+++ src/main/java/Capitulo05_arrays/bloque03/Ejercicio02_algoritmoBurbuja.java
@@ -7,7 +7,6 @@ public class Ejercicio02_algoritmoBurbuja {
 	public static void main(String[] args) {
 		int array[] = new int[10];
 		int auxiliar, auxiliar2,auxiliar3,i;
-		boolean SeIntercambian;
 		
 		for (i = 0; i < array.length; i++) {
 			array[i] = (int) Math.round(Math.random() * 100);
@@ -22,19 +21,18 @@ public class Ejercicio02_algoritmoBurbuja {
 		auxiliar = i;
 		for (int j = 1; j < (array.length-1); j++) {
 		if (j < auxiliar) {
-		auxiliar = j;
-		auxiliar2  = i;
-		i = auxiliar;
-		auxiliar = auxiliar2;
-		}
+		auxiliar2 = j;
+		array[auxiliar] = array[j];
+		array[auxiliar2]  = array[i];
+		array[auxiliar]= array[auxiliar2];
 		}
 		
 		System.out.print("2º Array: ");
-		for (i = 0; i < array.length; i++) {
-			System.out.print(array[i] + " ");
+		for (j = 0; j< array.length; i++) {
+			System.out.print(array[j] + " ");
 		}
 		System.out.println();
-		
-	}
+		}
+}
 
 }
