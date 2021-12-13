a1815df323fc07b8314d70521bcddf69bfcc4615
Equipo <Equipo@192.168.1.141>
2021-12-13 23:03:13 +0100

    bloque04

--- src/main/java/Capitulo05_arrays/bloque04/Ejercicio01_arrays.java
+++ src/main/java/Capitulo05_arrays/bloque04/Ejercicio01_arrays.java
@@ -3,18 +3,30 @@ package Capitulo05_arrays.bloque04;
 import java.util.Iterator;
 
 public class Ejercicio01_arrays {
-
-	public static void main(String[] args) {
-		int firstarray[] = new int [150];
-		int secondarray[] = new int [150];
-		int thirdarray[] = new int [150];
-		
-		
-		
-		
-		
-		
 	
-
+//	Crea una clase con un método “main” que cree un array de 150 posiciones y rellene el array con valores al azar entre 0 y 100
+// Calcula el valor medio de todos los elementos del array. Calcula el porcentaje de todos los números del array que están debajo del valor medio.
+	
+	public static void main(String[] args) {
+		int array[] = new int [150];
+		int i;
+		float media = 0, arrayTotal = 0, Porcentaje;
+		
+		for (i = 0; i < array.length; i++) {
+		array[i] = (int) Math.round(Math.random() * 100);
+		}
+		
+		for (i = 0; i < array.length; i++) {
+			arrayTotal = arrayTotal + array[i];
+		}
+		
+		for (i = 0; i < array.length; i++) {
+			media = (arrayTotal /(float)150);
+		}
+		
+		Porcentaje = (float) ((media*100)/ array.length);
+		
+		System.out.println("La media es : " + media);
+		System.out.println("El porcentaje es : " + Porcentaje + "%");
 }
 }
---
+++ src/main/java/Capitulo05_arrays/bloque04/Ejercicio02_arrays.java
@@ -0,0 +1,31 @@
+package Capitulo05_arrays.bloque04;
+
+import java.util.Iterator;
+
+public class Ejercicio02_arrays {
+	
+//	Crea una clase con un método “main” que cree 10 números al azar, almacenando 5 en un primer array y 5 para un segundo array. 
+// Muestra los arrays en pantalla usando un método que debes crear en esta clase. Crea un nuevo array de 5 posiciones,
+// que debes rellenar calculando la media de los números que ocupan la misma posición en los dos primeros arrays y 
+// almacenando dichas medias en la posición correspondiente del tercer array.
+// Muestra el tercer array en pantalla.
+
+	public static void main(String[] args) {
+		int array[] = new int [150];
+		int i;
+		
+		for (i = 0; i < array.length; i++) {
+		array[i] = (int) Math.round(Math.random() * 10 );
+		}
+
+		
+		
+		
+		
+		
+		
+		
+	
+
+}
+}
