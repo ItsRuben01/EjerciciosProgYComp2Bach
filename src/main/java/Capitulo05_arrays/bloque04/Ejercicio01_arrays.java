ec58e8cdd6bfa96c56fdeb0989b5196960da5a3c
ItsRuben01 <rubcastru@gmail.com>
2021-12-17 11:31:05 +0100

    bloque05

--- src/main/java/Capitulo05_arrays/bloque04/Ejercicio01_arrays.java
+++ src/main/java/Capitulo05_arrays/bloque04/Ejercicio01_arrays.java
@@ -4,8 +4,11 @@ import java.util.Iterator;
 
 public class Ejercicio01_arrays {
 	
-//	Crea una clase con un método “main” que cree un array de 150 posiciones y rellene el array con valores al azar entre 0 y 100
-// Calcula el valor medio de todos los elementos del array. Calcula el porcentaje de todos los números del array que están debajo del valor medio.
+/**													TITULO EJERCICIO 1
+ * @param Crea una clase con un método “main” que cree un array de 150 posiciones y rellene el array con valores al azar entre 0 y 100
+ *  Calcula el valor medio de todos los elementos del array. Calcula el porcentaje de todos los números del array que están debajo del valor medio.
+ */
+    
 	
 	public static void main(String[] args) {
 		int array[] = new int [150];
---
+++ src/main/java/Capitulo05_arrays/bloque05/Ejercicio01_arraysMatrices.java
@@ -0,0 +1,9 @@
+package Capitulo05_arrays.bloque05;
+
+public class Ejercicio01_arraysMatrices {
+
+	public static void main(String[] args) {
+
+	}
+
+}
