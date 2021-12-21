f9450011c8006c27386f1e7c9682d391f4d80463
ItsRuben01 <rubcastru@gmail.com>
2021-12-21 10:31:24 +0100

    bloque05

--- src/main/java/Capitulo05_arrays/bloque05/Ejercicio01_arraysMatrices.java
+++ src/main/java/Capitulo05_arrays/bloque05/Ejercicio01_arraysMatrices.java
@@ -4,6 +4,35 @@ public class Ejercicio01_arraysMatrices {
 
 	public static void main(String[] args) {
 
+	int matriz [][] = new int[][] {{1,0,0},
+								   {0,2,0},
+								   {0,0,3}};
+	
+	for (int i = 0; i < matriz.length; i++) {
+		for (int j = 0; j < matriz[i].length; j++) {
+							
+		}
+	}
+	System.out.println("Matriz es triangular: " + esMatrizTriangular(matriz));
+	}
+	
+	/**
+	 * 				
+	 * @param Es Matriz Triangular
+	 * @return
+	 */
+	public static boolean esMatrizTriangular (int matriz[][]) {
+		
+	for (int i = 0; i < matriz.length; i++) {
+		for (int j = 0; j < matriz[i].length; j++) {
+		
+		if(matriz[i = 0][j = 1] != 0 && matriz [i = 0][j = 2] != 0 ) {
+		return false;
+		}
+			
+		}
+		}
+	return true;
 	}
 
 }
