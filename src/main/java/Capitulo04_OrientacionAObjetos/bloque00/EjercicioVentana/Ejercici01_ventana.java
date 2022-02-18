b3972b670a63895a86cf514902d33fd8474425b0
ItsRuben01 <rubcastru@gmail.com>
2022-02-18 11:31:08 +0100

    bloque04

--- src/main/java/Capitulo04_OrientacionAObjetos/bloque00/EjercicioVentana/Ejercici01_ventana.java
+++ src/main/java/Capitulo04_OrientacionAObjetos/bloque00/EjercicioVentana/Ejercici01_ventana.java
@@ -1,8 +1,11 @@
 package Capitulo04_OrientacionAObjetos.bloque00.EjercicioVentana;
 
 import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
@@ -12,12 +15,18 @@ public class Ejercici01_ventana {
 
 	JFrame ventana = new JFrame();
 	ventana.setBounds(0,0,800,600);
-	ventana.setVisible(true);
 		
-	ventana.getContentPane().setBackground(Color.decode("#b40bba"));
-	//ventana.getContentPane(new BorderLayout());
-	JTextField jtf = new JTextField();
-	ventana.getContentPane().add(jtf);
+	//ventana.getContentPane().setBackground(Color.decode("#ffa600"));
+	ventana.getContentPane().setLayout(new BorderLayout());
+    ventana.setMinimumSize(new Dimension(400, 400));
+    
+    Canvas MiPropioCanvas = new Canvas();
+    
+	JCheckBox Check1 = new JCheckBox();
+	ventana.getContentPane().add(Check1, BorderLayout.WEST);
+	
+	
+	ventana.setVisible(true);
 	
 	}
 
