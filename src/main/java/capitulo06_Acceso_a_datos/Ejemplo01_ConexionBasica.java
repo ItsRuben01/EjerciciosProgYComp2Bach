dc649267adca1a55b1ca836a9e237273b5ca19c1
ItsRuben01 <rubcastru@gmail.com>
2022-03-29 10:29:52 +0200

    bloque06

--- src/main/java/capitulo06_Acceso_a_datos/Ejemplo01_ConexionBasica.java
+++ src/main/java/capitulo06_Acceso_a_datos/Ejemplo01_ConexionBasica.java
@@ -10,10 +10,63 @@ import java.util.Scanner;
 
 public class Ejemplo01_ConexionBasica {
 
+	
+
 	/**
-	 * 
+	 *  PRUEBA DELETE
 	 */
+	private static void pruebaDelete () {
+		int id;
+		String NuevoNombre;
+		String NuevoApellido;
+		String NuevoNif;
+		Scanner sc = new Scanner (System.in);
+		
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			// Cambiamos los datos
+			System.out.println("Introduce el id del registro afectado: ");
+			id = Integer.parseInt(sc.next());
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			int numeroderegistrosafectados = s.executeUpdate("DELETE from alumnos.alumno where id=" + id + ";");
+		   
+			System.out.println(numeroderegistrosafectados + "registros afectados");
+			
+			// Navegación del objeto ResultSet
+			
+			// Cierre de los elementos
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	/*
+	 * PRUEBA INSERT
+	 */
+	
 	private static void pruebaInsert () {
+
 		int id;
 		String NuevoNombre;
 		String NuevoApellido;
@@ -46,8 +99,8 @@ public class Ejemplo01_ConexionBasica {
 			
 			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
 			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
-			int numeroderegistrosafectados = s.executeUpdate("INSERT INTO alumnos.alumnoVALUES (" + id + "," + "'" + NuevoNombre 
-					+ "','" + NuevoApellido + "','" + NuevoNif + "')" );
+			int numeroderegistrosafectados = s.executeUpdate("INSERT INTO alumnos.alumno VALUES (" + id + ", '" + NuevoNombre + "', '" + 
+					NuevoApellido + "', '" + NuevoNif + "')" );
 		   
 			System.out.println(numeroderegistrosafectados + "registros afectados");
 			
@@ -66,6 +119,10 @@ public class Ejemplo01_ConexionBasica {
 			ex.printStackTrace();
 		}
 	}
+
+	/*
+	 * PRUEBA UPDATE
+	 */
 	
 	private static void pruebaUpdate () {
 		int id;
@@ -126,9 +183,7 @@ public class Ejemplo01_ConexionBasica {
 	 *  PRUEBA CONSULTA BÁSICA
 	 */
 
-	/**
-	 * 
-	 */
+	
 	private static void pruebaConsultaBasica () {
 		try {
 			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
@@ -168,16 +223,192 @@ public class Ejemplo01_ConexionBasica {
 			ex.printStackTrace();
 		}
 	}
+
 	/**
-	 * 
+	 *  PRUEBA 1º REGISTRO
 	 */
+	private static void pruebaprimerregistro () {
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			ResultSet rs = s.executeQuery ("select * from alumnos.alumno order by id limit 1");
+		   
+			// Navegación del objeto ResultSet
+			while (rs.next() == true) { 
+				System.out.println("id: " + rs.getInt("id") + " - nombre: " +
+						rs.getString("nombre") + " - apellidos: " + 
+						rs.getString("apellidos") + " - nif: " + 
+						rs.getString("nif"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
 	
 	/**
-	 * 
-	 * @param args
+	 *  PRUEBA ÚLTIMO REGISTRO
 	 */
+	private static void pruebaultimoregistro () {
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			ResultSet rs = s.executeQuery ("select * from alumnos.alumno order by id desc limit 1");
+		   
+			// Navegación del objeto ResultSet
+			while (rs.next() == true) { 
+				System.out.println("id: " + rs.getInt("id") + " - nombre: " +
+						rs.getString("nombre") + " - apellidos: " + 
+						rs.getString("apellidos") + " - nif: " + 
+						rs.getString("nif"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+
+	/**
+	 *  PRUEBA SIGUIENTE REGISTRO
+	 */
+	private static void pruebasiguienteregistro (int IdActual) {
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			ResultSet rs = s.executeQuery ("SELECT * from alumnos.alumno order by where id >"+ IdActual + "limit 1");
+		   
+			// Navegación del objeto ResultSet
+			while (rs.next() == true) { 
+				System.out.println("id: " + rs.getInt("id") + " - nombre: " +
+						rs.getString("nombre") + " - apellidos: " + 
+						rs.getString("apellidos") + " - nif: " + 
+						rs.getString("nif"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	/**
+	 *  PRUEBA ANTERIOR REGISTRO
+	 */
+	private static void pruebaanteriorregistro (int IdActual) {
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			ResultSet rs = s.executeQuery ("SELECT * from alumnos.alumno order by where id <"+ IdActual + "desc limit 1");
+		   
+			// Navegación del objeto ResultSet
+			while (rs.next() == true) { 
+				System.out.println("id: " + rs.getInt("id") + " - nombre: " +
+						rs.getString("nombre") + " - apellidos: " + 
+						rs.getString("apellidos") + " - nif: " + 
+						rs.getString("nif"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	/** 
+	 *  LLAMAR A EL MÉTODO
+	 */
+	
+
 	public static void main(String[] args) {
-		pruebaInsert();
+		 //pruebaInsert();
+		 //pruebaDelete();
+		 //pruebaultimoregistro();
+		 //pruebasiguienteregistro(1);
+			pruebaanteriorregistro(3);
 	}
 
 }
